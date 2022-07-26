69607646cd376dab21b3bd6c2b34fc2124e124f3
Marc Seibert <marc.seibert@1und1.de>
2022-07-26 15:52:47 +0200

    rm clipnotify add hello

--- flat/packages/hello.scm
+++ flat/packages/hello.scm
@@ -1,13 +1,13 @@
 (use-modules (gnu packages base))    ;for 'hello'
 
-(define clipnotify-1.0.22
+(define hello-2.2
   (package
-    (inherit clipnotify)
-    (version "1.0.2")
+    (inherit hello)
+    (version "2.2")
     (source (origin
               (method url-fetch)
-              ;(uri (string-append "mirror://gnu/hello/hello-" version
-              ;                    ".tar.gz"))
+              (uri (string-append "mirror://gnu/hello/hello-" version
+                                  ".tar.gz"))
               (sha256
                (base32
                 "0lappv4slgb5spyqbh6yl5r013zv72yqg2pcl30mginf3wdqd8k9"))))))
